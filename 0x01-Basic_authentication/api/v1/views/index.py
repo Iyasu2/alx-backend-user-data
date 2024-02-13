d27861e191f44de2f220d8ea6ad4523c69675c67
iyasu2 <iyasuforreal@gmail.com>
2024-02-13 18:38:57 +0300

    s

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """ Unauthorized endpoint """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ Forbidden endpoint """
+    abort(403)
