74f62c5a8c8383fbd77d950e09afe0d3de5f75d0
iyasu2 <iyasuforreal@gmail.com>
2024-02-13 18:52:53 +0300

    s

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,21 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to require auth """
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        # Add a trailing slash to path for uniformity
+        if not path.endswith('/'):
+            path += '/'
+
+        # Check if path is in excluded_paths
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to get authorization header """
