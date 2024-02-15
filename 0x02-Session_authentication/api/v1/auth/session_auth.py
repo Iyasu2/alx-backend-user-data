63f13141bdb7ad281bd337748a724ed7ab7cb2d4
iyasu2 <iyasuforreal@gmail.com>
2024-02-15 19:52:46 +0300

    s

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+this is the module
+'''
+from api.v1.auth.auth import Auth
+import base64
+from typing import TypeVar
+from models.user import User
+
+
+class SessionAuth(Auth):
+    '''
+    a class
+    '''
+    pass
