dcc3c1de4c037ecf44d7cec3265524c655b255cb
iyasu2 <iyasuforreal@gmail.com>
2024-02-15 21:19:08 +0300

    s

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,43 +2,59 @@
 '''
 this is the module
 '''
-from api.v1.auth.session_exp_auth import SessionExpAuth
+from flask import request
+from datetime import datetime, timedelta
+
 from models.user_session import UserSession
+from .session_exp_auth import SessionExpAuth
 
 
 class SessionDBAuth(SessionExpAuth):
     '''
-    this is a class
+    a class
     '''
-    def create_session(self, user_id=None):
+
+    def create_session(self, user_id=None) -> str:
         '''
         a function
         '''
         session_id = super().create_session(user_id)
-        if session_id is None:
-            return None
-        user_session = UserSession(user_id=user_id, session_id=session_id)
-        user_session.save()
-        return session_id
+        if type(session_id) == str:
+            kwargs = {
+                'user_id': user_id,
+                'session_id': session_id,
+            }
+            user_session = UserSession(**kwargs)
+            user_session.save()
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
         '''
         a function
         '''
-        user_session = UserSession.search({'session_id': session_id})
-        if user_session:
-            return user_session[0].user_id
-        return None
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(sessions) <= 0:
+            return None
+        cur_time = datetime.now()
+        time_span = timedelta(seconds=self.session_duration)
+        exp_time = sessions[0].created_at + time_span
+        if exp_time < cur_time:
+            return None
+        return sessions[0].user_id
 
-    def destroy_session(self, request=None):
+    def destroy_session(self, request=None) -> bool:
         '''
         a function
         '''
         session_id = self.session_cookie(request)
-        if session_id is None:
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
             return False
-        user_session = UserSession.search({'session_id': session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
-        return False
+        if len(sessions) <= 0:
+            return False
+        sessions[0].remove()
+        return True
--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,6 +10,9 @@ class UserSession(Base):
     this is a class
     '''
     def __init__(self, *args: list, **kwargs: dict):
+        '''
+        a function
+        '''
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
