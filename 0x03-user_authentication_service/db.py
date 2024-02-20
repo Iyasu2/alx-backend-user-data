9297b31330ebabd1384f80699d6a3b257d74c0e3
iyasu2 <iyasuforreal@gmail.com>
2024-02-20 19:32:55 +0300

    s

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -65,10 +65,16 @@ class DB:
             NoResultFound: Raised when no results are found.
             InvalidRequestError: Raised when wrong query arguments are passed.
         """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("No result found")
-            return user
-        except InvalidRequestError:
-            raise InvalidRequestError("Invalid query arguments")
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
